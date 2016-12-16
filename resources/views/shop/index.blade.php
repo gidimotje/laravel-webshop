0c29a677ee6b2369ebdfafeaee9f6e6626863cac
gidimotje <gidimotje.gvk@gmail.com>
2016-12-16 06:53:39 +0100

    Starting off

---
+++ resources/views/shop/index.blade.php
@@ -0,0 +1,108 @@
+@extends('layouts.master')
+
+@section('title')
+	Webshop - Ducare
+@endsection
+
+@section('content')
+	<div class="row">
+	  <div class="col-sm-6 col-md-4">
+	    <div class="thumbnail">
+	      <img src="https://html5box.com/html5lightbox/images/lakeandballoon.jpg" alt="...">
+	      <div class="caption">
+	        <h3>Product Title</h3>
+	        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	        consequat.</p>
+	        <div class="clearfix">
+	        	<div class="pull-left price">$15</div>
+	        	<a href="#" class="btn pull-right btn-success" role="button">Add to cart</a>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+	  <div class="col-sm-6 col-md-4">
+	    <div class="thumbnail">
+	      <img src="https://html5box.com/html5lightbox/images/lakeandballoon.jpg" alt="...">
+	      <div class="caption">
+	        <h3>Product Title</h3>
+	        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	        consequat.</p>
+	        <div class="clearfix">
+	        	<div class="pull-left price">$15</div>
+	        	<a href="#" class="btn pull-right btn-success" role="button">Add to cart</a>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+	  <div class="col-sm-6 col-md-4">
+	    <div class="thumbnail">
+	      <img src="https://html5box.com/html5lightbox/images/lakeandballoon.jpg" alt="...">
+	      <div class="caption">
+	        <h3>Product Title</h3>
+	        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	        consequat.</p>
+	        <div class="clearfix">
+	        	<div class="pull-left price">$15</div>
+	        	<a href="#" class="btn pull-right btn-success" role="button">Add to cart</a>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+	<div class="row">
+	  <div class="col-sm-6 col-md-4">
+	    <div class="thumbnail">
+	      <img src="https://html5box.com/html5lightbox/images/lakeandballoon.jpg" alt="...">
+	      <div class="caption">
+	        <h3>Product Title</h3>
+	        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	        consequat.</p>
+	        <div class="clearfix">
+	        	<div class="pull-left price">$15</div>
+	        	<a href="#" class="btn pull-right btn-success" role="button">Add to cart</a>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+	  <div class="col-sm-6 col-md-4">
+	    <div class="thumbnail">
+	      <img src="https://html5box.com/html5lightbox/images/lakeandballoon.jpg" alt="...">
+	      <div class="caption">
+	        <h3>Product Title</h3>
+	        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	        consequat.</p>
+	        <div class="clearfix">
+	        	<div class="pull-left price">$15</div>
+	        	<a href="#" class="btn pull-right btn-success" role="button">Add to cart</a>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+	  <div class="col-sm-6 col-md-4">
+	    <div class="thumbnail">
+	      <img src="https://html5box.com/html5lightbox/images/lakeandballoon.jpg" alt="...">
+	      <div class="caption">
+	        <h3>Product Title</h3>
+	        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	        consequat.</p>
+	        <div class="clearfix">
+	        	<div class="pull-left price">$15</div>
+	        	<a href="#" class="btn pull-right btn-success" role="button">Add to cart</a>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+@endsection
