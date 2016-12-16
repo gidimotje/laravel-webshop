58ce9f5d675d0ce2e5fba6f9d0ff4aa02be59ef0
gidimotje <gidimotje.gvk@gmail.com>
2016-12-16 17:04:31 +0100

    Payment functionality

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Product;
 use App\Cart;
 use Session;
+use Stripe\Stripe;
+use Stripe\Charge;
 
 class ProductController extends Controller
 {
@@ -46,5 +48,29 @@ class ProductController extends Controller
         $cart = new Cart($oldCart);
         $total = $cart->totalPrice;
         return view('shop.checkout', ['total' => $total]);
+    }
+
+    public function postCheckout(Request $request)
+    {
+        if(!Session::has('cart')){
+            return redirect()->route('shop.shoppingCart');
+        }
+        $oldCart = Session::get('cart');
+        $cart = new Cart($oldCart);
+        
+        Stripe::setApiKey('sk_test_LjzyPmWo1Z1Qvh52h1oKHQux');
+        try {
+            Charge::create(array(
+              "amount" => $cart->totalPrice * 100,
+              "currency" => "usd",
+              "source" => $request->input('stripeToken'), // obtained with Stripe.js
+              "description" => "Test charge"
+            ));
+        } catch (\Exeption $e) {
+            return redirect()->route('checkout')->with('error', $e->getMessage());
+        }
+
+        Session::forget('cart');
+        return redirect()->route('product.index')->with('success', 'Sucessfully purchased products!');
     }
 }
--- public/src/js/checkout.js
+++ public/src/js/checkout.js
@@ -6,11 +6,11 @@ $form.submit(function(event) {
 	$('#charge-error').addClass('hidden');
 	$form.find('button').prop('disabled', true);
 	Stripe.card.createToken({
-		number: $('#card-number').val();
-		cvc: $('#card-cvc').val();
-		exp_month: $('#card-expiry-month').val();
-		exp_year: $('#card-expiry-year').val();
-		name: $('#card-name').val();
+		number: $('#card-number').val(),
+		cvc: $('#card-cvc').val(),
+		exp_month: $('#card-expiry-month').val(),
+		exp_year: $('#card-expiry-year').val(),
+		name: $('#card-name').val()
 	}, stripeResponseHandler);
 	return false;
 });
