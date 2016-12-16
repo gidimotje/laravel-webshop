491a41834d76a86a44548106a6c44403c2299c1f
gidimotje <gidimotje.gvk@gmail.com>
2016-12-16 14:16:01 +0100

    Login logic

---
+++ resources/views/user/profile.blade.php
@@ -0,0 +1,14 @@
+@extends ('layouts.master')
+
+@section('title')
+	SignIn - Ducare
+@endsection
+
+@section('content')
+<div class="row">
+	<div class="col-md-4 col-md-offset-4">
+
+		<h1>User Profile</h1>
+	</div>
+</div>
+@endsection
